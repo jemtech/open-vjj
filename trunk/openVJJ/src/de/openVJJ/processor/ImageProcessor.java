dcc8175c22fd286d7ff18780356bc7db7f61f3a9
matthies.janerik@gmail.com <matthies.janerik@gmail.com@68e65340-157a-220b-c41f-846b6721652d>
2012-10-31 22:25:17 +0000

    add gnu gpl header

--- trunk/openVJJ/src/de/openVJJ/processor/ImageProcessor.java
+++ trunk/openVJJ/src/de/openVJJ/processor/ImageProcessor.java
@@ -6,6 +6,22 @@ import de.openVJJ.ImageListener.ImageListener;
 import de.openVJJ.graphic.VideoFrame;
 import de.openVJJ.imagePublisher.ImagePublisher;
 
+/*
+ * Copyright (C) 2012  Jan-Erik Matthies
+ *
+ * This program is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation;
+ * either version 3 of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with this program;
+ * if not, see <http://www.gnu.org/licenses/>.  
+ */
+
 public abstract class ImageProcessor extends ImagePublisher implements ImageListener {
 	
 	@Override
