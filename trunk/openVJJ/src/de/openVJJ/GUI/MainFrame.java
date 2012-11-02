c899b57f53a8a2621f26063a77f1cb67a7122ec5
matthies.janerik@gmail.com <matthies.janerik@gmail.com@68e65340-157a-220b-c41f-846b6721652d>
2012-11-02 18:45:46 +0000

    set visible and add gpl comment

--- trunk/openVJJ/src/de/openVJJ/GUI/MainFrame.java
+++ trunk/openVJJ/src/de/openVJJ/GUI/MainFrame.java
@@ -2,8 +2,25 @@ package de.openVJJ.GUI;
 
 import javax.swing.JFrame;
 
+/*
+ * Copyright (C) 2012  Jan-Erik Matthies
+ *
+ * This program is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation;
+ * either version 3 of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with this program;
+ * if not, see <http://www.gnu.org/licenses/>.  
+ */
+
 public class MainFrame extends JFrame{
 	public MainFrame() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
 	}
 }
