61dc3a33a98016f28d007b2e57376e3cfc6db032
matthies.janerik@googlemail.com <matthies.janerik@googlemail.com@68e65340-157a-220b-c41f-846b6721652d>
2013-12-18 15:31:25 +0000

    improof

--- openVJJ/src/de/openVJJ/graphic/ComplexLine.java
+++ openVJJ/src/de/openVJJ/graphic/ComplexLine.java
@@ -3,6 +3,23 @@ package de.openVJJ.graphic;
 import java.util.ArrayList;
 import java.util.List;
 
+
+/*
+ * Copyright (C) 2013  Jan-Erik Matthies
+ *
+ * This program is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation;
+ * either version 3 of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with this program;
+ * if not, see <http://www.gnu.org/licenses/>.  
+ */
+
 public class ComplexLine {
 	
 	private ComplexLinePoint start;
@@ -20,6 +37,11 @@ public class ComplexLine {
 		return true;
 	}
 	
+	/**
+	 * 
+	 * @param point
+	 * @return matching neighbours of this list
+	 */
 	public List<ComplexLinePoint> searchNeighbour(ComplexLinePoint point){
 		List<ComplexLinePoint> neighbours = new ArrayList<ComplexLinePoint>();
 		getNeighbour(start, point, neighbours);
@@ -80,7 +102,9 @@ public class ComplexLine {
 		if(point.hadSearched){
 			return false;
 		}
-		contains(point);
+		if(contains(point)){
+			return true;
+		}
 		point.hadSearched = true;
 		for(ComplexLinePoint neighbour : point.neighbours){
 			if(crossing(neighbour)){
@@ -90,6 +114,57 @@ public class ComplexLine {
 		return false;
 	}
 	
+	public boolean addLine(ComplexLine line){
+		List<ComplexLinePoint> crossings = getCrossingPoints(line);
+		if(crossings.size() < 1){
+			return false;
+		}
+		addWithout(crossings.get(0), crossings);
+		crossings.get(0).unsetSearched();
+		return true;
+	}
 	
+	private void addWithout(ComplexLinePoint point, List<ComplexLinePoint> without){
+		if(point.hadSearched){
+			return;
+		}
+
+		point.hadSearched = true;
+		
+		boolean thisNot = false;
+		for(ComplexLinePoint notMe : without){
+			if(point.equals(notMe)){
+				thisNot = true;
+				break;
+			}
+		}
+		if(!thisNot){
+			if(!addPoint(point)){
+				System.err.println("not able to add");
+			}
+		}
+		for(ComplexLinePoint neighbour : point.neighbours){
+			addWithout(neighbour, without);
+		}
+	}
 	
+	public List<ComplexLinePoint> getCrossingPoints(ComplexLine line){
+		List<ComplexLinePoint> matches = new ArrayList<ComplexLinePoint>();
+		getCrossingPoints(line.start, matches);
+		line.start.unsetSearched();
+		return matches;
+	}
+	
+	private void getCrossingPoints(ComplexLinePoint point, List<ComplexLinePoint> matches){
+		if(point.hadSearched){
+			return;
+		}
+		if(contains(point)){
+			matches.add(point);
+		}
+		point.hadSearched = true;
+		for(ComplexLinePoint neighbour : point.neighbours){
+			getCrossingPoints(neighbour, matches);
+		}
+	}
 }
